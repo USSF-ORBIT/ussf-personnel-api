f021339b04349ed919637e352e81995c8228b7f1
Jacob Capps <jcapps@truss.works>
2023-07-14 15:45:15 -0500

    Move everything back into one file

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./src/schema";
-import { resolvers } from "./src/resolvers/index";
+import { gql } from "graphql-tag";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+export const resolvers = {
+  Query: {
+    hello: () => "Hello World!",
+  },
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
