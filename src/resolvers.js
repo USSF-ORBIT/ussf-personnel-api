6135fdda6ff78e05663f3180ba22956314a7f323
Jacob Capps <jcapps@truss.works>
2023-07-19 11:22:52 -0500

    Remove toString()

--- src/resolvers.js
+++ src/resolvers.js
@@ -14,68 +14,49 @@ export const resolvers = {
         return {
           Grade: worksheet
             .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.Grade)
-            .value.toString(),
+            .getCell(enlistedUserColumns.Grade).value,
           DUTYTITLE: worksheet
             .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.DUTYTITLE)
-            .value.toString(),
-          AMU: worksheet
-            .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.AMU)
-            .value.toString(),
+            .getCell(enlistedUserColumns.DUTYTITLE).value,
+          AMU: worksheet.getRow(foundUserRow).getCell(enlistedUserColumns.AMU)
+            .value,
           DOD_ID: worksheet
             .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.DOD_ID)
-            .value.toString(),
+            .getCell(enlistedUserColumns.DOD_ID).value,
           ATP31: worksheet
             .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.ATP31)
-            .value.toString(),
+            .getCell(enlistedUserColumns.ATP31).value,
           AFC291_01: worksheet
             .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.AFC291_01)
-            .value.toString(),
-          AMF: worksheet
-            .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.AMF)
-            .value.toString(),
-          MPF: worksheet
-            .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.MPF)
-            .value.toString(),
+            .getCell(enlistedUserColumns.AFC291_01).value,
+          AMF: worksheet.getRow(foundUserRow).getCell(enlistedUserColumns.AMF)
+            .value,
+          MPF: worksheet.getRow(foundUserRow).getCell(enlistedUserColumns.MPF)
+            .value,
           MAJCOM: worksheet
             .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.MAJCOM)
-            .value.toString(),
+            .getCell(enlistedUserColumns.MAJCOM).value,
           Country: worksheet
             .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.Country)
-            .value.toString(),
+            .getCell(enlistedUserColumns.Country).value,
           BASE_LOC: worksheet
             .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.BASE_LOC)
-            .value.toString(),
+            .getCell(enlistedUserColumns.BASE_LOC).value,
           Org_type: worksheet
             .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.Org_type)
-            .value.toString(),
+            .getCell(enlistedUserColumns.Org_type).value,
           EOPDate: worksheet
             .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.EOPDate)
-            .value.toString(),
+            .getCell(enlistedUserColumns.EOPDate).value,
           Last_Name: worksheet
             .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.Last_Name)
-            .value.toString(),
+            .getCell(enlistedUserColumns.Last_Name).value,
           First_name: worksheet
             .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.First_name)
-            .value.toString(),
+            .getCell(enlistedUserColumns.First_name).value,
           Middle_Name: worksheet
             .getRow(foundUserRow)
-            .getCell(enlistedUserColumns.Middle_Name)
-            .value.toString(),
+            .getCell(enlistedUserColumns.Middle_Name).value,
         };
       }
       return null;
@@ -89,74 +70,52 @@ export const resolvers = {
         .values.indexOf(id);
       if (foundUserRow) {
         return {
-          ANB2: worksheet
-            .getRow(foundUserRow)
-            .getCell(officerUserColumns.ANB2)
-            .value.toString(),
+          ANB2: worksheet.getRow(foundUserRow).getCell(officerUserColumns.ANB2)
+            .value,
           DOD_ID: worksheet
             .getRow(foundUserRow)
-            .getCell(officerUserColumns.DOD_ID)
-            .value.toString(),
+            .getCell(officerUserColumns.DOD_ID).value,
           ATP31: worksheet
             .getRow(foundUserRow)
-            .getCell(officerUserColumns.ATP31)
-            .value.toString(),
-          CAS3: worksheet
-            .getRow(foundUserRow)
-            .getCell(officerUserColumns.CAS3)
-            .value.toString(),
-          AMF: worksheet
-            .getRow(foundUserRow)
-            .getCell(officerUserColumns.AMF)
-            .value.toString(),
+            .getCell(officerUserColumns.ATP31).value,
+          CAS3: worksheet.getRow(foundUserRow).getCell(officerUserColumns.CAS3)
+            .value,
+          AMF: worksheet.getRow(foundUserRow).getCell(officerUserColumns.AMF)
+            .value,
           Grade: worksheet
             .getRow(foundUserRow)
-            .getCell(officerUserColumns.Grade)
-            .value.toString(),
+            .getCell(officerUserColumns.Grade).value,
           DUTYTITLE: worksheet
             .getRow(foundUserRow)
-            .getCell(officerUserColumns.DUTYTITLE)
-            .value.toString(),
-          MPF: worksheet
-            .getRow(foundUserRow)
-            .getCell(officerUserColumns.MPF)
-            .value.toString(),
-          CMD: worksheet
-            .getRow(foundUserRow)
-            .getCell(officerUserColumns.CMD)
-            .value.toString(),
+            .getCell(officerUserColumns.DUTYTITLE).value,
+          MPF: worksheet.getRow(foundUserRow).getCell(officerUserColumns.MPF)
+            .value,
+          CMD: worksheet.getRow(foundUserRow).getCell(officerUserColumns.CMD)
+            .value,
           MAJCOM: worksheet
             .getRow(foundUserRow)
-            .getCell(officerUserColumns.MAJCOM)
-            .value.toString(),
+            .getCell(officerUserColumns.MAJCOM).value,
           Country: worksheet
             .getRow(foundUserRow)
-            .getCell(officerUserColumns.Country)
-            .value.toString(),
+            .getCell(officerUserColumns.Country).value,
           BASE_LOC: worksheet
             .getRow(foundUserRow)
-            .getCell(officerUserColumns.BASE_LOC)
-            .value.toString(),
+            .getCell(officerUserColumns.BASE_LOC).value,
           org_kind: worksheet
             .getRow(foundUserRow)
-            .getCell(officerUserColumns.org_kind)
-            .value.toString(),
+            .getCell(officerUserColumns.org_kind).value,
           EOPDate: worksheet
             .getRow(foundUserRow)
-            .getCell(officerUserColumns.EOPDate)
-            .value.toString(),
+            .getCell(officerUserColumns.EOPDate).value,
           Last_Name: worksheet
             .getRow(foundUserRow)
-            .getCell(officerUserColumns.Last_Name)
-            .value.toString(),
+            .getCell(officerUserColumns.Last_Name).value,
           First_name: worksheet
             .getRow(foundUserRow)
-            .getCell(officerUserColumns.First_name)
-            .value.toString(),
+            .getCell(officerUserColumns.First_name).value,
           Middle_Name: worksheet
             .getRow(foundUserRow)
-            .getCell(officerUserColumns.Middle_Name)
-            .value.toString(),
+            .getCell(officerUserColumns.Middle_Name).value,
         };
       }
       return null;
