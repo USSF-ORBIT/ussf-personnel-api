af81743fad52a7f63f2225bb08e2520e3641b4a2
John Gedeon <john@truss.works>
2023-09-14 14:15:33 -0700

    add sysinfo endpoint

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import { resolvers } from "./resolvers.js"
 import { typeDefs } from "./schema.js"
+import { enlistedFilename, officerFilename } from "./constants.js";
+import { getEnlistedWorksheet, getOfficerWorksheet } from './helpers.js';
 
 interface MyContext {
   token?: string;
@@ -46,6 +48,45 @@ app.use(
     context: async ({ req }) => ({ token: req.headers.token }),
   }),
 );
+
+// health check sysinfo
+const __BUILD_ID__ = process.env.BUILD_ID
+const __NODE_ENV__ = process.env.NODE_ENV
+const __VERSION__ = process.env.VERSION
+
+app.get('/api/sysinfo', async function (_, res) {
+  try {
+    // determine if we are using local file system or s3
+    const isUsingS3 = process.env.S3_BUCKET_NAME && process.env.S3_BUCKET_NAME !== "test_bucket"
+    // grab the last modified times
+    const { lastModifiedAt: officerLastUpdated } = await getOfficerWorksheet()
+    const { lastModifiedAt: enlistedLastUpdated } = await getEnlistedWorksheet()
+
+    res.status(200).json({
+      version: __VERSION__,
+      buildId: __BUILD_ID__,
+      nodeEnv: __NODE_ENV__,
+      isUsingS3,
+      enlisted: {
+        filename: enlistedFilename,
+        lastModifiedAt: enlistedLastUpdated,
+      },
+      officer: {
+        filename: officerFilename,
+        lastModifiedAt: officerLastUpdated,
+      },
+    })
+  } catch (e) {
+    console.error(e)
+    // still output json for status in event of a failure
+    res.status(500).send({
+      version: __VERSION__,
+      buildId: __BUILD_ID__,
+      nodeEnv: __NODE_ENV__,
+      error: 'unable to access spreadsheets'
+    })
+  }
+});
 
 // Modified server startup
 await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
